c8cc0c31c477a9c3d83d21722d739fa0290a49c5
puge <mail@puge.org>
2020-09-29 15:28:09 +0800

    save

--- dist/static/js/owo.core.js
+++ dist/static/js/owo.core.js
@@ -1,4 +1,4 @@
-// Mon Sep 28 2020 23:53:48 GMT+0800 (GMT+08:00)
+// Tue Sep 29 2020 15:28:01 GMT+0800 (GMT+08:00)
 var owo = {tool: {},state: {},};
 /* 方法合集 */
 var _owo = {
@@ -260,6 +260,108 @@ _owo.cutStringArray = function (original, before, after, index, inline) {
 
 
 // 页面切换
+
+_owo.animation = function (oldDom, newDom, animationIn, animationOut, forward) {
+  if (!oldDom || !newDom) {
+    console.error('错误的页面切换!', oldDom, newDom)
+    return
+  }
+  // 没有动画处理 如果没有某些必须方法也不使用动画(IE)
+  if (!animationIn || !animationOut || _owo.isIE) {
+    if (oldDom) {
+      // 隐藏掉旧的节点
+      oldDom.style.display = 'none'
+    }
+    // 查找页面跳转后的page
+    newDom.style.display = ''
+    return
+  }
+  if (typeof animationIn == 'string') animationIn = animationIn.split('&&')
+  if (typeof animationOut == 'string') animationOut = animationOut.split('&&')
+  // 动画延迟
+  var delay = 0
+  // 获取父元素
+  var parentDom = newDom.parentElement
+  if (!oldDom) {
+    console.error('旧页面不存在!')
+  }
+  oldDom.addEventListener("animationend", oldDomFun)
+  newDom.addEventListener("animationend", newDomFun)
+  
+  oldDom.style.position = 'absolute'
+
+  newDom.style.position = 'absolute'
+  newDom.style.display = ''
+  // 给即将生效的页面加上“未来”标识
+  if (forward) {
+    newDom.classList.add('owo-animation-forward')
+  } else {
+    oldDom.classList.add('owo-animation-forward')
+  }
+  // document.body.style.overflow = 'hidden'
+
+  parentDom.style.perspective = '1200px'
+  oldDom.classList.add('owo-animation')
+  for (var ind =0; ind < animationIn.length; ind++) {
+    var value = animationIn[ind]
+    //判断是否为延迟属性
+    if (value.slice(0, 5) == 'delay') {
+      var tempDelay = parseInt(value.slice(5))
+      if (delay < tempDelay)  delay = tempDelay
+    }
+    oldDom.classList.add('o-page-' + value)
+  }
+
+  newDom.classList.add('owo-animation')
+  for (var ind =0; ind < animationOut.length; ind++) {
+    var value = animationOut[ind]
+    if (value.slice(0, 5) == 'delay') {
+      var tempDelay = parseInt(value.slice(5))
+      if (delay < tempDelay)  delay = tempDelay
+    }
+    newDom.classList.add('o-page-' + value)
+  }
+  // 旧DOM执行函数
+  function oldDomFun (e) {
+    // 排除非框架引起的结束事件
+    // if (e.target.getAttribute('template') || e.target.getAttribute('route')) {
+      // 移除监听
+      oldDom.removeEventListener('animationend', oldDomFun, false)
+      // 延迟后再清除，防止动画还没完成
+      setTimeout(function () {
+        oldDom.style.display = 'none'
+        // console.log(oldDom)
+        oldDom.style.position = ''
+        oldDom.classList.remove('owo-animation')
+        oldDom.classList.remove('owo-animation-forward')
+        parentDom.style.perspective = ''
+        // 清除临时设置的class
+        for (var ind =0; ind < animationIn.length; ind++) {
+          var value = animationIn[ind]
+          oldDom.classList.remove('o-page-' + value)
+        }
+      }, delay);
+    // }
+  }
+
+  // 新DOM执行函数
+  function newDomFun () {
+    // 移除监听
+    newDom.removeEventListener('animationend', newDomFun, false)
+    // 延迟后再清除，防止动画还没完成
+    setTimeout(function () {
+      // 清除临时设置的style
+      newDom.style.position = '';
+      newDom.classList.remove('owo-animation');
+      newDom.classList.remove('owo-animation-forward');
+      for (var ind =0; ind < animationOut.length; ind++) {
+        var value = animationOut[ind]
+        newDom.classList.remove('o-page-' + value);
+      }
+    }, delay);
+  }
+  owo.state._animation = null
+}
 
 
 
@@ -637,6 +739,36 @@ function switchPage (oldUrlParam, newUrlParam) {
     // if (window.owo.script[newPage].view) _owo.getViewChange()
   }, 0)
 
+  
+  // 判断是否有动画效果
+  if (!owo.state._animation) owo.state._animation = {}
+  // 直接.in会在ie下报错
+  var animationIn = owo.state._animation['in']
+  var animationOut = owo.state._animation['out']
+  var forward = owo.state._animation['forward']
+  // 待优化 不需要这段代码的情况不打包这段代码
+  var isForward = window.owo.script[newPage]._index > window.owo.script[oldPage]._index
+  if (!animationIn && !animationOut) {
+    if (owo.globalAni) {
+      if (owo.globalAni["in"]) animationIn =  isForward ? owo.globalAni["in"] : owo.globalAni.backIn
+      if (owo.globalAni.out) animationOut = isForward ? owo.globalAni.out : owo.globalAni.backOut
+    }
+    if (owo.pageAni && owo.pageAni[owo.activePage]) {
+      if (owo.pageAni[owo.activePage]["in"]) animationIn = isForward ? owo.pageAni[owo.activePage]["in"] : owo.pageAni[owo.activePage].backIn
+      if (owo.pageAni[owo.activePage].out) animationOut = isForward ? owo.pageAni[owo.activePage].out : owo.pageAni[owo.activePage].backOut
+    }
+  }
+  // 全局跳转设置判断
+  if (owo.state.go) {
+    animationIn = animationIn || owo.state.go.inAnimation
+    animationOut = animationOut || owo.state.go.outAnimation
+    forward = forward || owo.state.go.forward
+  }
+  
+  if (animationIn || animationOut) {
+    _owo.animation(oldDom, newDom, animationIn.split('&&'), animationOut.split('&&'), forward)
+    return
+  }
   
   
   if (oldDom) {
